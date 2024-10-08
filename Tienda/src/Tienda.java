bfe4f96f9228f1242b8e69de3ea73c989bb91447
Shekny <videojuegosmorilla@hotmail.com>
2024-10-08 21:02:20 +0200

    Tienda

---
+++ Tienda/src/Tienda.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Tienda {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("\u001B[40m" +"\u001B[32m" +"Bienvenidos a Informasoda donde la Informatica siempre esta de Moda"+"\u001B[0m");
+        System.out.println("\u001B[32m");
+        System.out.println("        *");
+        System.out.println("       * *");
+        System.out.println("      * G *");
+        System.out.println("     *  A  *");
+        System.out.println("    *   M   *");
+        System.out.println("   *    E    *");
+        System.out.println("  *     R     *");
+        System.out.println(" *      S      *");
+        System.out.println("*               *");
+        System.out.println("* * * * * * * * *");
+        System.out.println("*       W       *");
+        System.out.println("*       E       *");
+        System.out.println("*       L       *");
+        System.out.println("*       C       *");
+        System.out.println("*       O       *");
+        System.out.println("*       M       *");
+        System.out.println("*       E       *");
+        System.out.println("* * * * * * * * *");
+        String Placabase = "Placa base";
+        String Grafica = "Grafica";
+        String Ram = "Ram";
+        String Refrigeracion = "Refrigeracion";
+        String Pantalla = "Pantalla";
+        double PPlacabase = 179.99;
+        double PGrafica = 400.8;
+        double PRam = 130.5;
+        double PRerfigeracion = 180;
+        double PPantalla = 200;
+        System.out.println("NUESTRO CATALOGO");
+        System.out.println("" +Placabase +"....." +PPlacabase);
+        System.out.println("" +Grafica +"....." +PGrafica);
+        System.out.println("" +Ram +"....." +PRam);
+        System.out.println("" +Refrigeracion +"....." +PRerfigeracion);
+        System.out.println("" +Pantalla +"....." +PPantalla);
+    }
+}
